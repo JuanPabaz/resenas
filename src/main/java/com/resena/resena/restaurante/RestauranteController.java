a4b12e52c622c95451b655f1a4c4ee28deaaa732
JuanPabaz <jpgthf@gmail.com>
2024-07-07 19:10:48 -0500

    Add Restaurante Controller and add Service annotation

---
+++ src/main/java/com/resena/resena/restaurante/RestauranteController.java
@@ -0,0 +1,23 @@
+package com.resena.resena.restaurante;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequestMapping("api/v2/restaurante")
+@RestController
+public class RestauranteController {
+
+    @Autowired
+    private RestauranteServiceImpl restauranteService;
+
+    @GetMapping("/find_all_restaurante")
+    public Optional<List<Restaurante>> findAllRestaurante(){
+        return restauranteService.findAllRestaurante();
+    }
+
+}
--- src/main/java/com/resena/resena/restaurante/RestauranteServiceImpl.java
+++ src/main/java/com/resena/resena/restaurante/RestauranteServiceImpl.java
@@ -1,10 +1,12 @@
 package com.resena.resena.restaurante;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class RestauranteServiceImpl implements IRestauranteService{
 
     @Autowired
